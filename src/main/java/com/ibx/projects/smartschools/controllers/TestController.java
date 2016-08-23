cd09f055836aa36167c3d3cba0aac54e1ed5f30b
rakesh <rak1994kumar@gmail.com>
2016-08-22 17:44:45 -0700

    findByAllParams Query Qorking in Class Subjects Table

--- src/main/java/com/ibx/projects/smartschools/controllers/TestController.java
+++ src/main/java/com/ibx/projects/smartschools/controllers/TestController.java
@@ -1,6 +1,6 @@
 package com.ibx.projects.smartschools.controllers;
 
-import java.util.List;
+import java.util.Optional;
 
 import javax.websocket.server.PathParam;
 
@@ -12,12 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.ibx.project.smartschools.exception.SmartSchoolException;
-import com.ibx.project.smartschools.service.SmartSchoolService;
 import com.ibx.projects.smartschools.models.Class_subject_table;
-import com.ibx.projects.smartschools.models.Class_table;
 import com.ibx.projects.smartschools.repositories.ClassSubjectRepository;
 
 
@@ -29,7 +24,7 @@ public class TestController {
 	@Autowired
 	private ClassSubjectRepository classSubjectRepository;
 	public TestController() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	@RequestMapping(value="/getClass",method = RequestMethod.GET)
 	public @ResponseBody Page<Class_subject_table> getAllClass(
@@ -38,6 +33,38 @@ public class TestController {
 			){
 		final PageRequest page1 = new PageRequest(0,5, Direction.ASC, "subject1");
 		Page<Class_subject_table>classList =  classSubjectRepository.findSubjectById(id,page1);
+		
+		return classList;
+		
+	}
+	
+	@RequestMapping(value="/getSubject",method = RequestMethod.GET)
+	public @ResponseBody Optional<Class_subject_table> getSubjects(
+			@PathParam("subjectName")String subjectName
+			){
+		
+		Optional<Class_subject_table>classList =  classSubjectRepository.findBysubject1(subjectName);
+		
+		return classList;
+		
+		  
+	}
+	
+	@RequestMapping(value="/findAll",method = RequestMethod.GET)
+	public @ResponseBody Page<Class_subject_table> findAll(
+			@PathParam("s1")String s1,
+			@PathParam("s2")String s2,
+			@PathParam("s3")String s3,
+			@PathParam("s4")String s4,
+			@PathParam("s5")String s5,
+			@PathParam("s6")String s6,
+			@PathParam("s7")String s7,
+			@PathParam("s8")String s8,
+			@PathParam("s9")String s9,
+			@PathParam("s10")String s10
+			){
+		final PageRequest page1 = new PageRequest(0,5, Direction.ASC, "subject1");
+		Page<Class_subject_table>classList =  classSubjectRepository.findByAllParams(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, page1);
 		
 		return classList;
 		
--- src/main/java/com/ibx/projects/smartschools/repositories/ClassSubjectRepository.java
+++ src/main/java/com/ibx/projects/smartschools/repositories/ClassSubjectRepository.java
@@ -14,6 +14,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ibx.projects.smartschools.models.Class_subject_table;
+import com.ibx.projects.smartschools.models.Class_table;
+
 import java.lang.Long;
 
 /**
@@ -27,7 +29,27 @@ public interface ClassSubjectRepository extends JpaRepository<Class_subject_tabl
 	 @Query("SELECT t.subject1,t.subject2,t.subject1 FROM Class_subject_table t where t.id = :id") 
 	 Page<Class_subject_table> findSubjectById(@Param("id") Long id, Pageable pagable);
 	 
-	// Class_subject_table findByClass_subjects_id(Long class_subjects_id);
+	 @Query("Select cst from Class_subject_table as cst where ( cst.subject1 LIKE %:s1% or :s1 is '%%' or :s1 is null ) AND "
+				+ "( cst.subject2 LIKE %:s2% or :s2 is '%%' or :s2 is null ) AND "
+				+ "( cst.subject3 LIKE %:s3% or :s3 is '%%' or :s3 is null ) AND "
+				+ "( cst.subject4 LIKE %:s4% or :s4 is '%%' or :s4 is null ) AND "
+				+ "( cst.subject5 LIKE %:s5% or :s5 is '%%' or :s5 is null ) AND "
+				+ "( cst.subject6 LIKE %:s6% or :s6 is '%%' or :s6 is null ) AND "
+				+ "( cst.subject7 LIKE %:s7% or :s7 is '%%' or :s7 is null ) AND "
+				+ "( cst.subject8 LIKE %:s8% or :s8 is '%%' or :s8 is null ) AND "
+				+ "( cst.subject9 LIKE %:s9% or :s9 is '%%' or :s9 is null ) AND "
+				+ "( cst.subject10 LIKE %:s10% or :s10 is '%%' or :s10 is null ) ")
+		Page<Class_subject_table> findByAllParams(
+				@Param("s1") String  s1,
+				@Param("s2") String  s2,
+				@Param("s3") String  s3,
+				@Param("s4") String  s4,
+				@Param("s5") String  s5,
+				@Param("s6") String  s6,
+				@Param("s7") String  s7,
+				@Param("s8") String  s8,
+				@Param("s9") String  s9,
+				@Param("s10") String s10,Pageable pageable);
      
 	 Optional<Class_subject_table> findBysubject1(String subjectName);
 	 
