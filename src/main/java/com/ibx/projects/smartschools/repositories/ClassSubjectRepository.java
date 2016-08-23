aec63d0211f5466aee48c4c775c4108cceab74a5
rakesh <rak1994kumar@gmail.com>
2016-08-22 17:12:08 -0700

    additional query added

--- src/main/java/com/ibx/projects/smartschools/repositories/ClassSubjectRepository.java
+++ src/main/java/com/ibx/projects/smartschools/repositories/ClassSubjectRepository.java
@@ -4,6 +4,7 @@
 package com.ibx.projects.smartschools.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +14,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ibx.projects.smartschools.models.Class_subject_table;
+import java.lang.Long;
 
 /**
  * @author SANTOSH
@@ -24,6 +26,10 @@ public interface ClassSubjectRepository extends JpaRepository<Class_subject_tabl
 	
 	 @Query("SELECT t.subject1,t.subject2,t.subject1 FROM Class_subject_table t where t.id = :id") 
 	 Page<Class_subject_table> findSubjectById(@Param("id") Long id, Pageable pagable);
+	 
+	// Class_subject_table findByClass_subjects_id(Long class_subjects_id);
+     
+	 Optional<Class_subject_table> findBysubject1(String subjectName);
 	 
 	Page<Class_subject_table> findAll(Pageable pageable);
 
